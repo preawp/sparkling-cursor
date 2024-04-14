dea9ce7e80430abf60cbb9b52f05499cd33aae43
preawp <preawp@bu.edu>
2024-04-14 16:28:02 -0400

    working

--- src/useSparklingCursor.js
+++ src/useSparklingCursor.js
@@ -1,21 +1,31 @@
 import { useEffect } from 'react';
 
-/* const gold  =  ['#FFD700', '#FFA500', '#FFD55A', '#EAC086', '#FFFAF0'] */
-function useSparklingCursor({ colorPalette = ['#ab75d5', '#92d7d2', '#95d987', '#edd46b', '#FFFAF0'], symbol = '✦', size = '12px', duration = 2000, count = 5 } = {}) {
+function useSparklingCursor({
+                                colorPalette =  ['#FFD700', '#FFA500', '#FFD55A', '#EAC086', '#FFFAF0']
+                                ,
+                                symbol = '+',
+                                size = '14px',
+                                duration = 2000,
+                                count = 5,
+                                glowEnabled = false,
+                                glowColor = '#fff4c3'
+                            } = {}) {
     useEffect(() => {
         const handleMouseMove = event => {
             for (let i = 0; i < count; i++) {
                 const sparkle = document.createElement('div');
-                sparkle.textContent = '+';
+                sparkle.textContent = symbol;
                 sparkle.style.position = 'absolute';
                 sparkle.style.left = `${event.pageX + Math.random() * 20 - 10}px`;
                 sparkle.style.top = `${event.pageY + Math.random() * 20 - 10}px`;
                 sparkle.style.color = colorPalette[Math.floor(Math.random() * colorPalette.length)];
                 sparkle.style.fontSize = size;
-                sparkle.style.opacity = 1;
-               /*sparkle.style.textShadow = '0 0 8px #ffffcc';*/
-                sparkle.style.transition = `opacity ${duration / 500}s ease-out, transform ${duration / 1000}s ease-out`;
-                sparkle.style.transform = 'scale(1)';
+                sparkle.style.opacity = Math.random() * 0.5 + 0.5;
+                if (glowEnabled) {
+                    sparkle.style.textShadow = `0 0 8px ${glowColor}`;
+                }
+                sparkle.style.transition = `opacity ${duration / 1000}s ease-out, transform ${duration / 1000}s ease-out`;
+                sparkle.style.transform = `scale(${Math.random() * 0.5 + 0.5})`;
                 document.body.appendChild(sparkle);
 
                 setTimeout(() => {
@@ -34,7 +44,7 @@ function useSparklingCursor({ colorPalette = ['#ab75d5', '#92d7d2', '#95d987', '
         return () => {
             window.removeEventListener('mousemove', handleMouseMove);
         };
-    }, [colorPalette, symbol, size, duration, count]);
+    }, [colorPalette, symbol, size, duration, count, glowEnabled, glowColor]);
 }
 
 export default useSparklingCursor;
